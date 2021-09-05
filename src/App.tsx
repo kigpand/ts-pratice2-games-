5f62dacf77f5ec1e0cc84565b5925728a71bcc94
kigpand <kigpand@naver.com>
2021-09-05 22:20:56 +0900

    가위바위보게임 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import RSP from './components/RSP';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <TodoList />
+      <RSP />
     </div>
   );
 }
